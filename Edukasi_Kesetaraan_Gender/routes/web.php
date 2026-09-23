b9c687f46baae7505b7ed2e1a2fe7ebf974a83d1
Muhammad Rizqi Naufal Lithany <254311052@pnm.ac.id>
2026-09-23 22:53:38 +0700

    feat(modul1-backend): daftarkan route auth, sudah dites tinker

--- Edukasi_Kesetaraan_Gender/routes/web.php
+++ Edukasi_Kesetaraan_Gender/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [RegisterController::class, 'create'])->name('register');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+Route::post('/login', [LoginController::class, 'store'])->name('login.store');
